713299925a611e96fb1ac0088e2482081ccf9eec
Michal Halabica <m.halabica@gmail.com>
2021-05-19 21:23:04 +0200

    Základ repository patternu a vložení do kontejneru.

---
+++ src/GrillBot/GrillBot.Database/DatabaseExtensions.cs
@@ -0,0 +1,25 @@
+﻿using GrillBot.Database.Services;
+using GrillBot.Database.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace GrillBot.Database
+{
+    static public class DatabaseExtensions
+    {
+        static public IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
+        {
+            services
+                .AddDbContext<GrillBotContext>(b => b.UseNpgsql(connectionString));
+
+            var repositories = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(o => typeof(RepositoryBase).IsAssignableFrom(o))
+                .ToList();
+
+            repositories.ForEach(repo => services.AddScoped(repo));
+            return services;
+        }
+    }
+}
---
+++ src/GrillBot/GrillBot.Database/Services/Repositories/RepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace GrillBot.Database.Services.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        protected GrillBotContext Context { get; }
+
+        protected RepositoryBase(GrillBotContext context)
+        {
+            Context = context;
+        }
+    }
+}

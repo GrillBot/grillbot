af8e7416401b35865022af022602d8cb8c1abf9a
Michal Halabica <m.halabica@gmail.com>
2021-05-22 17:30:49 +0200

    Oprava oprávnění DMs

--- src/GrillBot/GrillBot.App/Modules/ServerManagementModule.cs
+++ src/GrillBot/GrillBot.App/Modules/ServerManagementModule.cs
@@ -1,17 +1,16 @@
 ﻿using Discord;
 using Discord.Commands;
-using GrillBot.App.Infrastructure;
 using GrillBot.App.Infrastructure.Preconditions;
 using GrillBot.Data;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GrillBot.App.Modules
 {
     [CommandEnabledCheck("Nelze provést příkaz ze skupiny správy serveru, protože je deaktivován.")]
+    [RequireContext(ContextType.Guild, ErrorMessage = "Tento příkaz lze provést jen na serveru.")]
     public class ServerManagementModule : Infrastructure.ModuleBase
     {
         private IConfiguration Configuration { get; }
@@ -23,6 +22,10 @@ namespace GrillBot.App.Modules
 
         [Command("clean")]
         [Summary("Smaže zprávy v příslušném kanálu. Pokud nebyl zadán kanál jako parametr, tak bude použit kanál, kde byl zavolán příkaz.")]
+        [RequireBotPermission(GuildPermission.ManageMessages, ErrorMessage = "Nemohu mazat zprávy, protože nemám oprávnění na mazání zpráv.")]
+        [RequireBotPermission(GuildPermission.AddReactions, ErrorMessage = "Nemohu provést tento příkaz, protože nemám oprávnění přidat reakce indikující stav.")]
+        [RequireBotPermission(GuildPermission.ReadMessageHistory, ErrorMessage = "Nemohu mazat zprávy, protože nemám oprávnění na čtení historie.")]
+        [RequireUserPermission(GuildPermission.ManageMessages, ErrorMessage = "Tento příkaz může použít pouze uživatel, který má oprávnění mazat zprávy.")]
         public async Task CleanAsync([Name("pocet")] int take, [Name("kanal")] ITextChannel channel = null)
         {
             await Context.Message.AddReactionAsync(Emote.Parse(Configuration["Discord:Emotes:Loading"]));

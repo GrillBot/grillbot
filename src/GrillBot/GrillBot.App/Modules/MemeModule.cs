1ef804bf09ef977d98ab835d5a7d2cb44a65d0e9
Michal Halabica <m.halabica@gmail.com>
2021-06-20 10:08:14 +0200

    Peepoangry image

--- src/GrillBot/GrillBot.App/Modules/MemeModule.cs
+++ src/GrillBot/GrillBot.App/Modules/MemeModule.cs
@@ -4,6 +4,7 @@ using GrapeCity.Documents.Imaging;
 using GrillBot.App.Extensions;
 using GrillBot.App.Extensions.Discord;
 using GrillBot.App.Services.FileStorage;
+using GrillBot.Data.Resources.Peepoangry;
 using GrillBot.Data.Resources.Peepolove;
 using System;
 using System.Drawing;
@@ -87,7 +88,7 @@ namespace GrillBot.App.Modules
                     frame.Save(file.FullName, SysDraw.Imaging.ImageFormat.Png);
                 }
             }
-            catch (Exception) { withException = true; }
+            catch (Exception) { withException = true; throw; }
             finally
             {
                 if (File.Exists(file.FullName) && !withException)
@@ -108,6 +109,94 @@ namespace GrillBot.App.Modules
             graphics.DrawImage(hands, new Rectangle(0, 0, 512, 512));
 
             return (body as SysDraw.Image).CropImage(new Rectangle(0, 115, 512, 397));
+        }
+
+        #endregion
+
+        #region Peepoangry
+
+        [Command("peepoangry")]
+        [Alias("angry")]
+        [Summary("Naštvaně zírající peepo.")]
+        public async Task PeepoangryAsync([Name("id/tag/jmeno_uzivatele")] IUser user = null)
+        {
+            if (user == null) user = Context.User;
+            var cache = FileStorageFactory.CreateCache();
+
+            var filename = user.CreateProfilePicFilename(64);
+            var file = await cache.GetFileInfoAsync("Peepoangry", filename);
+            bool withException = false;
+
+            try
+            {
+                if (file.Exists) return;
+
+                var profilePictureImage = await cache.GetProfilePictureInfoAsync(filename);
+                if (!profilePictureImage.Exists)
+                    await cache.StoreProfilePictureAsync(filename, await user.DownloadAvatarAsync(size: 64));
+
+                if (file.Extension == ".gif" && profilePictureImage.Length > 2 * ((Context.Guild.CalculateFileUploadLimit() * 1024 * 1024) / 3))
+                {
+                    filename = Path.ChangeExtension(filename, ".png");
+                    file = await cache.GetFileInfoAsync("Peepoangry", filename);
+                    if (file.Exists) return;
+                }
+
+                using var profilePicture = SysDraw.Image.FromFile(profilePictureImage.FullName);
+                if (file.Extension == ".gif")
+                {
+                    using var gifWriter = new GcGifWriter(file.FullName);
+                    using var bitmap = new GcBitmap();
+                    var delayTime = profilePicture.CalculateGifDelay();
+
+                    foreach (var userFrame in profilePicture.SplitGifIntoFrames())
+                    {
+                        try
+                        {
+                            using var roundedUserFrame = userFrame.RoundImage();
+                            using var frame = RenderPeepoangryFrame(roundedUserFrame);
+
+                            using var ms = new MemoryStream();
+                            frame.Save(ms, SysDraw.Imaging.ImageFormat.Png);
+
+                            bitmap.Load(ms.ToArray());
+                            gifWriter.AppendFrame(bitmap, disposalMethod: GifDisposalMethod.RestoreToBackgroundColor, delayTime: delayTime);
+                        }
+                        finally
+                        {
+                            userFrame.Dispose();
+                        }
+                    }
+                }
+                else if (file.Extension == ".png")
+                {
+                    using var rounded = profilePicture.RoundImage();
+                    using var resized = rounded.ResizeImage(64, 64);
+
+                    using var frame = RenderPeepoangryFrame(resized);
+                    frame.Save(file.FullName, SysDraw.Imaging.ImageFormat.Png);
+                }
+            }
+            catch (Exception) { withException = true; throw; }
+            finally
+            {
+                if (File.Exists(file.FullName) && !withException)
+                    await ReplyFileAsync(file.FullName, false);
+            }
+        }
+
+        private static SysDraw.Image RenderPeepoangryFrame(SysDraw.Image profilePicture)
+        {
+            using var peepoangryStream = new MemoryStream(PeepoangryResources.peepoangry);
+
+            var body = new Bitmap(250, 105);
+            using var graphics = Graphics.FromImage(body);
+
+            graphics.DrawImage(profilePicture, new Rectangle(20, 10, 64, 64));
+            using var peepoangry = new Bitmap(peepoangryStream);
+            graphics.DrawImage(peepoangry, new Point(115, -5));
+
+            return body;
         }
 
         #endregion

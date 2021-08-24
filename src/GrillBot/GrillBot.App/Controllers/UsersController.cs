ea62d3a784cd43f59d336c31595ee3dba53c6048
Michal Halabica <m.halabica@gmail.com>
2021-08-24 21:29:38 +0200

    Implemented user update and points incrementation rest api

--- src/GrillBot/GrillBot.App/Controllers/UsersController.cs
+++ src/GrillBot/GrillBot.App/Controllers/UsersController.cs
@@ -3,12 +3,12 @@ using GrillBot.App.Extensions.Discord;
 using GrillBot.Data.Models.API;
 using GrillBot.Data.Models.API.Common;
 using GrillBot.Data.Models.API.Users;
+using GrillBot.Database.Enums;
 using GrillBot.Database.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NSwag.Annotations;
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -77,6 +77,65 @@ namespace GrillBot.App.Controllers
             var user = await DiscordClient.FindUserAsync(id);
             var detail = new UserDetail(entity, user);
             return Ok(detail);
+        }
+
+        /// <summary>
+        /// Updates user.
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="400">Validation failed.</response>
+        /// <response code="404">User not found</response>
+        [HttpPut("{id}")]
+        [OpenApiOperation(nameof(UsersController) + "_" + nameof(UpdateUserAsync))]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(MessageResponse), (int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<UserDetail>> UpdateUserAsync(ulong id, UpdateUserParams parameters)
+        {
+            var user = await DbContext.Users.AsQueryable()
+                .FirstOrDefaultAsync(o => o.Id == id.ToString());
+
+            if (user == null)
+                return NotFound(new MessageResponse("Zadaný uživatel nebyl nalezen."));
+
+            user.ApiToken = parameters.ApiToken;
+            user.Note = parameters.Note;
+
+            if (parameters.BotAdmin)
+                user.Flags |= (int)UserFlags.BotAdmin;
+            else
+                user.Flags &= ~(int)UserFlags.BotAdmin;
+
+            await DbContext.SaveChangesAsync();
+            return await GetUserDetailAsync(id);
+        }
+
+        /// <summary>
+        /// Increment points for user on server.
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="400">Validation failed</response>
+        /// <response code="404">User not found or user is not found in guild.</response>
+        [HttpPost("{userId}/{guildId}")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(MessageResponse), (int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> IncrementPointsAsync(ulong userId, ulong guildId, [Required] long amount)
+        {
+            var user = await DbContext.Users.AsQueryable()
+                .Include(o => o.Guilds)
+                .FirstOrDefaultAsync(o => o.Id == userId.ToString());
+
+            if (user == null)
+                return NotFound(new MessageResponse("Zadaný uživatel nebyl nalezen."));
+
+            var guildUser = user.Guilds.FirstOrDefault(o => o.GuildId == guildId.ToString());
+            if (guildUser == null)
+                return NotFound(new MessageResponse($"Uživatel {user.Username} nemá záznam o přítomnosti na zadaném serveru."));
+
+            guildUser.Points += amount;
+            await DbContext.SaveChangesAsync();
+            return Ok();
         }
     }
 }
---
+++ src/GrillBot/GrillBot.Data/Models/API/Users/UpdateUserParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GrillBot.Data.Models.API.Users
+{
+    public class UpdateUserParams
+    {
+        public Guid? ApiToken { get; set; }
+        public bool BotAdmin { get; set; }
+        public string Note { get; set; }
+    }
+}

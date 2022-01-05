98283d59e5604f71b2b9a5b5ae8c9c097ec8a1b8
Michal Halabica <m.halabica@gmail.com>
2022-01-05 17:04:18 +0100

    Implemented remind create as interaction command

---
+++ src/GrillBot/GrillBot.App/Modules/Interactions/RemindModule.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.Interactions;
+using GrillBot.App.Services.Reminder;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace GrillBot.App.Modules.Interactions;
+
+public class RemindModule : Infrastructure.InteractionsModuleBase
+{
+    private RemindService RemindService { get; }
+
+    public RemindModule(RemindService remindService)
+    {
+        RemindService = remindService;
+    }
+
+    [SlashCommand("remind", "Vytvoření připomenutí k určitému datu.")]
+    [RequireBotPermission(ChannelPermission.AddReactions)]
+    public async Task CreateAsync(
+        [Summary("kdo", "Označení uživatele, který si přeje dostat upozornění")]
+        IUser who,
+        [Summary("kdy", "Datum a čas události. Musí být v budoucnosti.")]
+        DateTime at,
+        [Summary("zprava", "Zpráva, která se zašle uživateli.")]
+        string message
+    )
+    {
+        try
+        {
+            var originalMessage = await Context.Interaction.GetOriginalResponseAsync();
+            await RemindService.CreateRemindAsync(Context.User, who, at, message, originalMessage);
+            await SetResponseAsync("Upozornění vytvořeno.");
+        }
+        catch (ValidationException ex)
+        {
+            await SetResponseAsync(ex.Message);
+        }
+    }
+}

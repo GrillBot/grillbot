521ed3e93822721ffc09210d7a5fb374c97a783e
Michal Halabica <m.halabica@gmail.com>
2022-01-25 19:33:37 +0100

    New slash command Kasparek. Resolved #116

---
+++ src/GrillBot/GrillBot.App/Modules/Interactions/MemeModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord.Interactions;
+using GrillBot.App.Services;
+
+namespace GrillBot.App.Modules.Interactions;
+
+public class MemeModule : Infrastructure.InteractionsModuleBase
+{
+    private RandomizationService RandomizationService { get; }
+
+    public MemeModule(RandomizationService randomizationService)
+    {
+        RandomizationService = randomizationService;
+    }
+
+    [SlashCommand("kasparek", "Zeptá se tvojí mámy, jakého máš kašpárka.")]
+    public Task GetRandomLengthAsync()
+    {
+        var random = RandomizationService.GetOrCreateGenerator("Kasparek");
+        var value = random.Next(0, 50);
+        return SetResponseAsync($"{value}cm");
+    }
+}

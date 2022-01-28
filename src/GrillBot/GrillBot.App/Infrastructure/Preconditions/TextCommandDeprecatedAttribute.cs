7def07c55423b9b7acf789d7f8248db3cb332b0d
Michal Halabica <m.halabica@gmail.com>
2022-01-28 18:03:17 +0100

    Implemented precondition attribute for commands migrated to interactions. Resolved #130

---
+++ src/GrillBot/GrillBot.App/Infrastructure/Preconditions/TextCommandDeprecatedAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+
+namespace GrillBot.App.Infrastructure.Preconditions
+{
+    public class TextCommandDeprecatedAttribute : PreconditionAttribute
+    {
+        public string AlternativeCommand { get; set; }
+
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            var message = "Tento příkaz již není v textové formě podporován." + (!string.IsNullOrEmpty(AlternativeCommand) ? $" Příkaz byl nahrazen příkazem `{AlternativeCommand}`" : "");
+            return Task.FromResult(PreconditionResult.FromError(message));
+        }
+    }
+}

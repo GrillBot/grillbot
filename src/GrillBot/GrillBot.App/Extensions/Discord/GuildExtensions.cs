895809935154cd0bad3ea7d36e3e0a6d60344836
Michal Halabica <m.halabica@gmail.com>
2022-02-14 22:41:06 +0100

    Review of list of guild features and update of DC.NET. Resolved #146

--- src/GrillBot/GrillBot.App/Extensions/Discord/GuildExtensions.cs
+++ src/GrillBot/GrillBot.App/Extensions/Discord/GuildExtensions.cs
@@ -4,6 +4,41 @@ namespace GrillBot.App.Extensions.Discord
 {
     static public class GuildExtensions
     {
+        private static Dictionary<string, string> FeaturesList { get; } = new Dictionary<string, string>()
+        {
+            { "AnimatedIcon", "Animovaná ikona" },
+            { "Banner", "Banner" },
+            { "Commerce", "eKomerce" },
+            { "Community", "Komunitní režim" },
+            { "Discoverable", "Veřejně viditelný" },
+            { "InviteSplash", "Pozadí u pozvánky" },
+            { "MemberVerificationGateEnabled", "Verifikace při připojení" },
+            { "News", "Novinky" },
+            { "Partnered", "Partnerský program" },
+            { "PreviewEnabled", "Náhled serveru před připojením" },
+            { "VanityUrl", "Vanity URL" },
+            { "Verified", "Ověřený server" },
+            { "VIPRegions", "VIP hlasová oblast" },
+            { "WelcomeScreenEnabled", "Uvítací obrazovka" },
+            { "MonetizationEnabled", "Monetizace" },
+            { "MoreStickers", "Nálepky" },
+            { "PrivateThreads", "Privátní vlákna" },
+            { "RoleIcons", "Ikony rolí" },
+            { "SevenDayThreadArchive", "Archivace vláken po týdnu" },
+            { "ThreeDayThreadArchive", "Archivace vláken po 3 dnech" },
+            { "TicketedEventsEnabled", "Události" },
+            { "AnimatedBanner", "Animovaný banner" },
+            { "TextInVoiceEnabled", "Text v hlasových kanálech" },
+            { "ThreadsEnabled", "Vlákna" },
+            { "ChannelBanner", "Banner kanálů" },
+            { "Hub", "Školní server" },
+            { "MoreEmoji", "Více emotů" },
+            { "RoleSubscriptionsAvailableForPurchase", "Placené role" },
+            { "MemberProfiles", "Alternativní profily uživatelů" },
+            { "NewThreadPermissions", "" },
+            { "ThreadsEnabledTesting", "" }
+        };
+
         static public IRole GetHighestRole(this IGuild guild, bool requireColor = false)
         {
             var roles = requireColor ? guild.Roles.Where(o => o.Color != Color.Default) : guild.Roles.AsEnumerable();
@@ -14,81 +49,15 @@ namespace GrillBot.App.Extensions.Discord
         static public IEnumerable<string> GetTranslatedFeatures(this IGuild guild)
         {
             if (guild.Features.Value == GuildFeature.None)
-                yield break;
+                return Enumerable.Empty<string>();
 
-            var features = Enum.GetValues<GuildFeature>()
-                .Where(o => o > 0 && guild.Features.HasFeature(o));
-
-            foreach (var feature in features)
-            {
-                switch (feature)
-                {
-                    case GuildFeature.AnimatedIcon:
-                        yield return "Animovaná ikona";
-                        break;
-                    case GuildFeature.Banner:
-                        yield return "Banner";
-                        break;
-                    case GuildFeature.Commerce:
-                        yield return "eKomerce";
-                        break;
-                    case GuildFeature.Community:
-                        yield return "Komunitní režim";
-                        break;
-                    case GuildFeature.Discoverable:
-                        yield return "Veřejně viditelný";
-                        break;
-                    case GuildFeature.InviteSplash:
-                        yield return "Pozadí u pozvánky";
-                        break;
-                    case GuildFeature.MemberVerificationGateEnabled:
-                        yield return "Verifikace při připojení";
-                        break;
-                    case GuildFeature.News:
-                        yield return "Novinky";
-                        break;
-                    case GuildFeature.Partnered:
-                        yield return "Partnerský program";
-                        break;
-                    case GuildFeature.PreviewEnabled:
-                        yield return "Náhled serveru před připojením.";
-                        break;
-                    case GuildFeature.VanityUrl:
-                        yield return "Vanity URL";
-                        break;
-                    case GuildFeature.Verified:
-                        yield return "Ověřený server";
-                        break;
-                    case GuildFeature.VIPRegions:
-                        yield return "VIP hlasová oblast";
-                        break;
-                    case GuildFeature.WelcomeScreenEnabled:
-                        yield return "Uvítací obrazovka";
-                        break;
-                    case GuildFeature.MonetizationEnabled:
-                        yield return "Monetizace";
-                        break;
-                    case GuildFeature.MoreStickers:
-                        yield return "Nálepky";
-                        break;
-                    case GuildFeature.PrivateThreads:
-                        yield return "Privátní vlákna";
-                        break;
-                    case GuildFeature.RoleIcons:
-                        yield return "Ikony rolí";
-                        break;
-                    case GuildFeature.SevenDayThreadArchive:
-                        yield return "Archivace vláken po týdnu";
-                        break;
-                    case GuildFeature.ThreeDayThreadArchive:
-                        yield return "Archivace vláken po 3 dnech";
-                        break;
-                    case GuildFeature.TicketedEventsEnabled:
-                        yield return "Události";
-                        break;
-                }
-            }
-
+            return Enum.GetValues<GuildFeature>()
+                .Where(o => o > 0 && guild.Features.HasFeature(o))
+                .Select(o => o.ToString())
+                .Select(o => FeaturesList.TryGetValue(o, out string text) ? text : o)
+                .Where(o => !string.IsNullOrEmpty(o))
+                .Distinct()
+                .OrderBy(o => o);
         }
 
         static public int CalculateFileUploadLimit(this IGuild guild)

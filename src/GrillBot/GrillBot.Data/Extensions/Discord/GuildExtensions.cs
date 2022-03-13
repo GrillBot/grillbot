5a3e57e9198d092ff3abd9c9d32df3042df0c10e
Michal Halabica <m.halabica@gmail.com>
2022-03-13 20:05:42 +0100

    Calculation of max upload limit

--- src/GrillBot/GrillBot.Data/Extensions/Discord/GuildExtensions.cs
+++ src/GrillBot/GrillBot.Data/Extensions/Discord/GuildExtensions.cs
@@ -4,11 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace GrillBot.Data.Extensions.Discord
+namespace GrillBot.Data.Extensions.Discord;
+
+static public class GuildExtensions
 {
-    static public class GuildExtensions
-    {
-        private static Dictionary<string, string> FeaturesList { get; } = new Dictionary<string, string>()
+    private static Dictionary<string, string> FeaturesList { get; } = new Dictionary<string, string>()
         {
             { "AnimatedIcon", "Animovaná ikona" },
             { "Banner", "Banner" },
@@ -43,49 +43,41 @@ namespace GrillBot.Data.Extensions.Discord
             { "ThreadsEnabledTesting", "" }
         };
 
-        static public IRole GetHighestRole(this IGuild guild, bool requireColor = false)
-        {
-            var roles = requireColor ? guild.Roles.Where(o => o.Color != Color.Default) : guild.Roles.AsEnumerable();
+    static public IRole GetHighestRole(this IGuild guild, bool requireColor = false)
+    {
+        var roles = requireColor ? guild.Roles.Where(o => o.Color != Color.Default) : guild.Roles.AsEnumerable();
 
-            return roles.OrderByDescending(o => o.Position).FirstOrDefault();
-        }
+        return roles.OrderByDescending(o => o.Position).FirstOrDefault();
+    }
 
-        static public IEnumerable<string> GetTranslatedFeatures(this IGuild guild)
-        {
-            if (guild.Features.Value == GuildFeature.None)
-                return Enumerable.Empty<string>();
+    static public IEnumerable<string> GetTranslatedFeatures(this IGuild guild)
+    {
+        if (guild.Features.Value == GuildFeature.None)
+            return Enumerable.Empty<string>();
 
-            return Enum.GetValues<GuildFeature>()
-                .Where(o => o > 0 && guild.Features.HasFeature(o))
-                .Select(o => o.ToString())
-                .Select(o => FeaturesList.TryGetValue(o, out string text) ? text : o)
-                .Where(o => !string.IsNullOrEmpty(o))
-                .Distinct()
-                .OrderBy(o => o);
-        }
+        return Enum.GetValues<GuildFeature>()
+            .Where(o => o > 0 && guild.Features.HasFeature(o))
+            .Select(o => o.ToString())
+            .Select(o => FeaturesList.TryGetValue(o, out string text) ? text : o)
+            .Where(o => !string.IsNullOrEmpty(o))
+            .Distinct()
+            .OrderBy(o => o);
+    }
 
-        static public int CalculateFileUploadLimit(this IGuild guild)
-        {
-            return guild?.PremiumTier switch
-            {
-                PremiumTier.Tier2 => 50,
-                PremiumTier.Tier3 => 100,
-                _ => 8
-            };
-        }
+    static public int CalculateFileUploadLimit(this IGuild guild)
+        => Convert.ToInt32((guild?.MaxUploadLimit ?? 0) / 1000000);
 
-        static public IEnumerable<SocketTextChannel> GetAvailableTextChannelsFor(this SocketGuild guild, SocketGuildUser user)
-        {
-            return guild.TextChannels
-                .Where(o => o is not SocketThreadChannel && o.HaveAccess(user));
-        }
+    static public IEnumerable<SocketTextChannel> GetAvailableTextChannelsFor(this SocketGuild guild, SocketGuildUser user)
+    {
+        return guild.TextChannels
+            .Where(o => o is not SocketThreadChannel && o.HaveAccess(user));
+    }
 
-        static public IEnumerable<SocketGuildChannel> GetAvailableChannelsFor(this SocketGuild guild, SocketGuildUser user)
-        {
-            return GetAvailableTextChannelsFor(guild, user)
-                .Select(o => o as SocketGuildChannel)
-                .Where(o => o != null)
-                .Concat(guild.VoiceChannels.Where(o => o.HaveAccess(user)));
-        }
+    static public IEnumerable<SocketGuildChannel> GetAvailableChannelsFor(this SocketGuild guild, SocketGuildUser user)
+    {
+        return GetAvailableTextChannelsFor(guild, user)
+            .Select(o => o as SocketGuildChannel)
+            .Where(o => o != null)
+            .Concat(guild.VoiceChannels.Where(o => o.HaveAccess(user)));
     }
 }

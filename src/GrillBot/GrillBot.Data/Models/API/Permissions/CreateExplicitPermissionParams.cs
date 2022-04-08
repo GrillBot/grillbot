f3579c344664f0ffbc53433643ead20b4bf27fed
Michal Halabica <m.halabica@gmail.com>
2022-04-08 18:06:08 +0200

    Validation for explicit permissions creation

--- src/GrillBot/GrillBot.Data/Models/API/Permissions/CreateExplicitPermissionParams.cs
+++ src/GrillBot/GrillBot.Data/Models/API/Permissions/CreateExplicitPermissionParams.cs
@@ -1,17 +1,19 @@
 ﻿using GrillBot.Database.Enums;
 using System.ComponentModel.DataAnnotations;
 
-namespace GrillBot.Data.Models.API.Permissions
+namespace GrillBot.Data.Models.API.Permissions;
+
+public class CreateExplicitPermissionParams
 {
-    public class CreateExplicitPermissionParams
-    {
-        [Required]
-        public string Command { get; set; }
+    [Required]
+    public string Command { get; set; }
 
-        public bool IsRole { get; set; }
+    [Required]
+    public bool IsRole { get; set; }
 
-        public string TargetId { get; set; }
+    [Required]
+    public string TargetId { get; set; }
 
-        public ExplicitPermissionState State { get; set; }
-    }
+    [Required]
+    public ExplicitPermissionState State { get; set; }
 }

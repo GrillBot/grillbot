4ef8b2e5f8ca4b9e0fffd902af7a4de66c3d77e0
Michal Halabica <m.halabica@gmail.com>
2022-05-20 14:01:21 +0200

    Added missing files

---
+++ src/GrillBot/GrillBot.Tests/Infrastructure/Cache/TestCacheBuilder.cs
@@ -0,0 +1,40 @@
+﻿using GrillBot.Cache.Services;
+using GrillBot.Cache.Services.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GrillBot.Tests.Infrastructure.Cache;
+
+[ExcludeFromCodeCoverage]
+public class TestCacheBuilder : GrillBotCacheBuilder
+{
+    public TestCacheBuilder() : base(null)
+    {
+    }
+
+    public static GrillBotCacheContext CreateContext()
+    {
+        var options = new DbContextOptionsBuilder<GrillBotCacheContext>()
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging()
+            .EnableThreadSafetyChecks()
+            .UseInMemoryDatabase("GrillBot-Cache")
+            .Options;
+
+        return new GrillBotCacheContext(options);
+    }
+
+    public override GrillBotCacheRepository CreateRepository()
+    {
+        var context = CreateContext();
+        return new GrillBotCacheRepository(context);
+    }
+
+    public static void ClearDatabase(GrillBotCacheRepository repository)
+    {
+        repository.RemoveCollection(repository.DirectApiRepository.GetAll());
+        repository.RemoveCollection(repository.MessageIndexRepository.GetMessagesAsync().Result);
+
+        repository.Commit();
+    }
+}

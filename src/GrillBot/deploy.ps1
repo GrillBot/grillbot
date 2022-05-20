3fc1b5b7202835cf9eb45e754a03423ee2d2b2fb
Michal Halabica <m.halabica@gmail.com>
2022-05-20 13:51:20 +0200

    Updated docker deployment

--- src/GrillBot/deploy.ps1
+++ src/GrillBot/deploy.ps1
@@ -1,10 +1,20 @@
 dotnet test -v quiet --nologo -l:"console;verbosity=normal";
-
 if(!($?)) {
     Write-Error -Message "Unit tests failed. Push and deployment cancelled.";
     exit;
 }
 
+git pull --rebase;
+if(!($?)) {
+    Write-Error -Message "Git pull (Rebase) failed. Check repository.";
+    exit;
+}
+
 git push;
+if(!($?)) {
+    Write-Error -Message "Git push failed. Check repository";
+    git push;
+}
+
 docker build -t registry.gitlab.com/grillbot/grillbot .
 docker push registry.gitlab.com/grillbot/grillbot

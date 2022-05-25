a149edc68ef1382859934fe7b4aa7976d9fa5e20
Michal Halabi <m.halabica@gmail.com>
2022-05-25 20:07:23 +0200

    ManagerExtensions

---
+++ src/GrillBot/GrillBot.Common/ManagersExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GrillBot.Common.Managers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GrillBot.Common;
+
+public static class ManagersExtensions
+{
+    public static IServiceCollection AddCommonManagers(this IServiceCollection services)
+    {
+        return services
+            .AddSingleton<InitManager>();
+    }
+}

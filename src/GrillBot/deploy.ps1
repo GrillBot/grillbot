9971870bf3cdd23256db00ea89e261321cdc0ab2
Michal Halabi <m.halabica@gmail.com>
2022-06-23 18:51:47 +0200

    Pull changes from git before test in deployment script

--- src/GrillBot/deploy.ps1
+++ src/GrillBot/deploy.ps1
@@ -1,12 +1,12 @@
-dotnet test -v quiet --nologo -l:"console;verbosity=normal";
-if(!($?)) {
-    Write-Error -Message "Unit tests failed. Push and deployment cancelled.";
-    exit;
-}
-
 git pull --rebase;
 if(!($?)) {
     Write-Error -Message "Git pull (Rebase) failed. Check repository.";
+    exit;
+}
+
+dotnet test -v quiet --nologo -l:"console;verbosity=normal";
+if(!($?)) {
+    Write-Error -Message "Unit tests failed. Push and deployment cancelled.";
     exit;
 }
 

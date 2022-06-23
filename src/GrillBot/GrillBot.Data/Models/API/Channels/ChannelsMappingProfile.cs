f27a3a308aeecf92d817d84f535995a23b66a50a
Michal Halabi <m.halabica@gmail.com>
2022-06-23 17:30:53 +0200

    Fixed bug in getting datetime of last message

--- src/GrillBot/GrillBot.Data/Models/API/Channels/ChannelsMappingProfile.cs
+++ src/GrillBot/GrillBot.Data/Models/API/Channels/ChannelsMappingProfile.cs
@@ -28,7 +28,7 @@ public class ChannelsMappingProfile : AutoMapper.Profile
             .ForMember(dst => dst.LastMessageAt, opt =>
             {
                 opt.PreCondition(src => !src.IsCategory() && src.Users.Count > 0);
-                opt.MapFrom(src => src.Users.Min(o => o.LastMessageAt));
+                opt.MapFrom(src => src.Users.Max(o => o.LastMessageAt));
             })
             .ForMember(dst => dst.MessagesCount, opt =>
             {

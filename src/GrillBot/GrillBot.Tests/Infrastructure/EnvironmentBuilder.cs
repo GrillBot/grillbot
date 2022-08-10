b242ca1fc0ba45948674dae4702907d8ae9a9160
Michal Halabi <m.halabica@gmail.com>
2022-08-11 00:24:25 +0200

    Fixed unit tests

--- src/GrillBot/GrillBot.Tests/Infrastructure/EnvironmentBuilder.cs
+++ src/GrillBot/GrillBot.Tests/Infrastructure/EnvironmentBuilder.cs
@@ -4,12 +4,6 @@ namespace GrillBot.Tests.Infrastructure;
 
 public class EnvironmentBuilder : BuilderBase<IWebHostEnvironment>
 {
-    public EnvironmentBuilder()
-    {
-        Mock.Setup(o => o.ApplicationName).Returns("GrillBot-Test");
-    }
-
-    public EnvironmentBuilder AsDev() => SetName("Development");
     public EnvironmentBuilder AsProd() => SetName("Production");
     public EnvironmentBuilder AsTest() => SetName("Testing");
 

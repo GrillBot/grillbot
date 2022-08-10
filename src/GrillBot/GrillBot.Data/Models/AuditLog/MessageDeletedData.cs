f60859b16f35ca3fc4c107ea506911b14ed35d6a
Michal Halabi <m.halabica@gmail.com>
2022-08-11 00:19:59 +0200

    Removed an unused flag about deleting a message that was not in the cache.

--- src/GrillBot/GrillBot.Data/Models/AuditLog/MessageDeletedData.cs
+++ src/GrillBot/GrillBot.Data/Models/AuditLog/MessageDeletedData.cs
@@ -4,17 +4,11 @@ namespace GrillBot.Data.Models.AuditLog;
 
 public class MessageDeletedData
 {
-    public bool Cached { get; set; }
     public MessageData Data { get; set; }
 
     public MessageDeletedData() { }
 
-    public MessageDeletedData(bool cached)
-    {
-        Cached = cached;
-    }
-
-    public MessageDeletedData(MessageData data) : this(data != null)
+    public MessageDeletedData(MessageData data)
     {
         Data = data;
     }

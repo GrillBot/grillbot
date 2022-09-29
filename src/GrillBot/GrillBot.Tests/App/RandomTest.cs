adf301c5de26c81191fa687bade9aff1b7ebd6e3
Michal Halabica <m.halabica@gmail.com>
2022-09-29 18:47:37 +0200

    Test of fonts

---
+++ src/GrillBot/GrillBot.Tests/App/RandomTest.cs
@@ -0,0 +1,21 @@
+﻿using ImageMagick;
+
+namespace GrillBot.Tests.App;
+
+[TestClass]
+public class RandomTest
+{
+    [TestMethod]
+    public void OpenSans()
+    {
+        using var img = new MagickImage(MagickColors.White, 500, 500);
+        new Drawables().Font("Open Sans").Text(100,100, "Text").Draw(img);
+    }
+
+    [TestMethod]
+    public void Arial()
+    {
+        using var img = new MagickImage(MagickColors.White, 500, 500);
+        new Drawables().Font("Arial").Text(100,100, "Text").Draw(img);
+    }
+}

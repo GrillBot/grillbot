0761a759dc500546a11c1c2e1b33bb16d1c2f0cc
Michal Halabica <m.halabica@gmail.com>
2022-10-28 22:07:24 +0200

    Removed unused constructors

--- src/GrillBot/GrillBot.Data/Models/AuditLog/CommandExecution.cs
+++ src/GrillBot/GrillBot.Data/Models/AuditLog/CommandExecution.cs
@@ -1,5 +1,4 @@
-﻿using Discord;
-using Discord.Commands;
+﻿using Discord.Commands;
 
 namespace GrillBot.Data.Models.AuditLog;
 
@@ -12,23 +11,4 @@ public class CommandExecution
     public string ErrorReason { get; set; }
     public int Duration { get; set; }
     public string Exception { get; set; }
-
-    public CommandExecution() { }
-
-    public CommandExecution(CommandInfo command, IMessage message, IResult result, int duration)
-    {
-        Command = command.Aliases[0];
-        MessageContent = message.Content;
-        Duration = duration;
-
-        if (result == null) 
-            return;
-
-        IsSuccess = result.IsSuccess;
-        CommandError = result.Error;
-        ErrorReason = result.ErrorReason;
-
-        if (!result.IsSuccess && result is ExecuteResult { Exception: { } } executeResult)
-            Exception = executeResult.Exception.ToString();
-    }
 }

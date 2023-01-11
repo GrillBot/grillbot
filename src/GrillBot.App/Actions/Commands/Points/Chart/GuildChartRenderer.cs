d41c9babedbaf6548e7da9e60fa3660ff3311d21
Michal Halabica <m.halabica@gmail.com>
2023-01-12 00:22:05 +0100

    Renderers in DI

--- src/GrillBot.App/Actions/Commands/Points/Chart/GuildChartRenderer.cs
+++ src/GrillBot.App/Actions/Commands/Points/Chart/GuildChartRenderer.cs
@@ -10,32 +10,32 @@ public class GuildChartRenderer
 {
     private ITextsManager Texts { get; }
     private IGraphicsClient GraphicsClient { get; }
-    private string Locale { get; }
 
-    public GuildChartRenderer(ITextsManager texts, IGraphicsClient graphicsClient, string locale)
+    public GuildChartRenderer(ITextsManager texts, IGraphicsClient graphicsClient)
     {
         Texts = texts;
-        Locale = locale;
         GraphicsClient = graphicsClient;
     }
 
-    public async Task<MagickImage> RenderAsync(IGuild guild, IEnumerable<(DateTime day, int messagePoints, int reactionPoints)> data, ChartsFilter filter)
+    public async Task<MagickImage> RenderAsync(IGuild guild, IEnumerable<(DateTime day, int messagePoints, int reactionPoints)> data, ChartsFilter filter,
+        string locale)
     {
-        var request = CreateRequest(filter, data, guild);
+        var request = CreateRequest(filter, data, guild, locale);
         var graphData = await GraphicsClient.CreateChartAsync(request);
 
         return new MagickImage(graphData);
     }
 
-    private ChartRequestData CreateRequest(ChartsFilter filter, IEnumerable<(DateTime day, int messagePoints, int reactionPoints)> data, IGuild guild)
+    private ChartRequestData CreateRequest(ChartsFilter filter, IEnumerable<(DateTime day, int messagePoints, int reactionPoints)> data, IGuild guild,
+        string locale)
     {
         var request = ChartRequestBuilder.CreateCommonRequest();
 
         request.Data.TopLabel!.Text = filter switch
         {
-            ChartsFilter.Messages => Texts["Points/Chart/Title/Guild/Messages", Locale],
-            ChartsFilter.Reactions => Texts["Points/Chart/Title/Guild/Reactions", Locale],
-            _ => Texts["Points/Chart/Title/Guild/Summary", Locale]
+            ChartsFilter.Messages => Texts["Points/Chart/Title/Guild/Messages", locale],
+            ChartsFilter.Reactions => Texts["Points/Chart/Title/Guild/Reactions", locale],
+            _ => Texts["Points/Chart/Title/Guild/Summary", locale]
         };
 
         var filteredData = ChartRequestBuilder.FilterData(data, filter).ToList();

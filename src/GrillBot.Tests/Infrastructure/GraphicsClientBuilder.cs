24e75ef0a9dc8824ea1a69c5f75ce6ce9ebd0c68
Michal Halabica <m.halabica@gmail.com>
2023-01-13 21:33:03 +0100

    Unit tests

---
+++ src/GrillBot.Tests/Infrastructure/GraphicsClientBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using GrillBot.App.Services.Graphics;
+using GrillBot.App.Services.Graphics.Models.Chart;
+using ImageMagick;
+using Moq;
+
+namespace GrillBot.Tests.Infrastructure;
+
+public class GraphicsClientBuilder : BuilderBase<IGraphicsClient>
+{
+    public GraphicsClientBuilder SetAll()
+        => SetCreateChartAction();
+    
+    public GraphicsClientBuilder SetCreateChartAction()
+    {
+        using var image = new MagickImage(MagickColors.White, 100, 100);
+        new Drawables().Line(0, 50, 50, 0).Draw(image);
+        var bytes = image.ToByteArray(MagickFormat.Png);
+
+        Mock.Setup(o => o.CreateChartAsync(It.IsAny<ChartRequestData>())).ReturnsAsync(bytes);
+        return this;
+    }
+}

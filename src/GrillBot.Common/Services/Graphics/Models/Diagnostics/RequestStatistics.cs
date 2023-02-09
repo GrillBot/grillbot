944311b2d10c5e39a247f564575c8ecce32c6ce4
Michal Halabica <m.halabica@gmail.com>
2023-02-09 22:51:37 +0100

    Graphics service duration statistics

--- src/GrillBot.Common/Services/Graphics/Models/Diagnostics/RequestStatistics.cs
+++ src/GrillBot.Common/Services/Graphics/Models/Diagnostics/RequestStatistics.cs
@@ -5,4 +5,7 @@ public class RequestStatistics
     public string Endpoint { get; set; } = null!;
     public long Count { get; set; }
     public DateTime LastRequestAt { get; set; }
+    public int TotalTime { get; set; }
+    public int AvgTime { get; set; }
+    public int LastTime { get; set; }
 }

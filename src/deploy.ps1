5d373b06523a18753ad0f7323810823b05aa3916
Michal Halabica <m.halabica@gmail.com>
2023-03-03 20:49:43 +0100

    Dropped unit tests in CI

--- src/deploy.ps1
+++ src/deploy.ps1
@@ -4,13 +4,6 @@ if(!($?)) {
     exit;
 }
 
-docker build -t grillbot-test-image -f ./Dockerfile.Test .
-if(!($?)) {
-    Write-Error -Message "Tests failed. Push and deployment cancelled.";
-    exit;
-}
-
-docker rmi $(docker images --format "{{.Repository}}:{{.Tag}}" | findstr 'grillbot-test-image')
 git push;
 if(!($?)) {
     Write-Error -Message "Git push failed. Check repository";

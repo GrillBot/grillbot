e097e3ee64ef8581496f038438f6ed41422f041b
Michal Halabica <m.halabica@gmail.com>
2023-03-13 17:18:42 +0100

    Removed old Points column

---
+++ src/GrillBot.Database/Migrations/20230313161818_RemovedPointsColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrillBot.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedPointsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "GuildUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Points",
+                table: "GuildUsers",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}

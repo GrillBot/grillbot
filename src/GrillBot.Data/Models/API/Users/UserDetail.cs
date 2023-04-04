a39d8130d5908585ca6ccc464f53541da90e29ca
Michal Halabica <m.halabica@gmail.com>
2023-04-04 18:15:00 +0200

    Do not return deleted channels in the public admin

--- src/GrillBot.Data/Models/API/Users/UserDetail.cs
+++ src/GrillBot.Data/Models/API/Users/UserDetail.cs
@@ -2,21 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using GrillBot.Database.Enums;
 
 namespace GrillBot.Data.Models.API.Users;
 
 public class UserDetail
 {
-    public string Id { get; set; }
-    public string Username { get; set; }
-    public string Discriminator { get; set; }
+    public string Id { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Discriminator { get; set; } = null!;
     public long Flags { get; set; }
     public bool HaveBirthday { get; set; }
     public List<GuildUserDetail> Guilds { get; set; } = new();
     public UserStatus Status { get; set; }
     public List<string> ActiveClients { get; set; } = new();
     public bool IsKnown { get; set; }
-    public string AvatarUrl { get; set; }
+    public string AvatarUrl { get; set; } = null!;
     public TimeSpan? SelfUnverifyMinimalTime { get; set; }
     public DateTime? RegisteredAt { get; set; }
     public string? Language { get; set; }
@@ -27,7 +28,8 @@ public class UserDetail
 
         foreach (var guild in Guilds)
         {
-            guild.Channels = guild.Channels.Where(o => !o.Channel.Name.StartsWith("Imported")).ToList();
+            guild.Channels = guild.Channels
+                .FindAll(o => !o.Channel.Name.StartsWith("Imported") && (o.Channel.Flags & (long)ChannelFlag.Deleted) == 0);
         }
     }
 }

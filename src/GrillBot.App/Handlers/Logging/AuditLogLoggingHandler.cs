a4e9285bdc35c7ace46c1812d004b9ca8722951c
Michal Halabica <m.halabica@gmail.com>
2023-06-07 22:35:39 +0200

    MergeStats and AuditLogLoggingHandler

--- src/GrillBot.App/Handlers/Logging/AuditLogLoggingHandler.cs
+++ src/GrillBot.App/Handlers/Logging/AuditLogLoggingHandler.cs
@@ -1,53 +1,81 @@
-﻿using GrillBot.App.Managers;
+﻿using AuditLogService.Models.Request;
+using GrillBot.App.Infrastructure.Jobs;
 using GrillBot.Common.Exceptions;
 using GrillBot.Common.Helpers;
 using GrillBot.Common.Managers.Logging;
-using GrillBot.Data.Models.AuditLog;
-using GrillBot.Database.Enums;
+using GrillBot.Common.Services.AuditLog;
+using GrillBot.Common.Services.AuditLog.Enums;
+using GrillBot.Common.Services.AuditLog.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GrillBot.App.Handlers.Logging;
 
 public class AuditLogLoggingHandler : ILoggingHandler
 {
-    private AuditLogWriteManager AuditLogWriteManager { get; }
     private IConfiguration Configuration { get; }
+    private IServiceProvider ServiceProvider { get; }
 
-    public AuditLogLoggingHandler(AuditLogWriteManager auditLogWriteManager, IConfiguration configuration)
+    public AuditLogLoggingHandler(IConfiguration configuration, IServiceProvider serviceProvider)
     {
-        AuditLogWriteManager = auditLogWriteManager;
         Configuration = configuration.GetSection("Discord:Logging");
+        ServiceProvider = serviceProvider;
     }
 
     public Task<bool> CanHandleAsync(LogSeverity severity, string source, Exception? exception = null)
     {
-        if (exception == null || !Configuration.GetValue<bool>("Enabled")) return Task.FromResult(false);
+        if (exception is null || !Configuration.GetValue<bool>("Enabled")) return Task.FromResult(false);
         return Task.FromResult(severity is LogSeverity.Critical or LogSeverity.Error or LogSeverity.Warning);
     }
 
     public Task InfoAsync(string source, string message) => Task.CompletedTask;
 
     public async Task WarningAsync(string source, string message, Exception? exception = null)
-    {
-        var isWarning = exception != null && LoggingHelper.IsWarning(exception);
-        var data = CreateWrapper(isWarning, source, message, exception);
-        await AuditLogWriteManager.StoreAsync(data);
-    }
+        => await ErrorAsync(source, message, exception);
 
     public async Task ErrorAsync(string source, string message, Exception? exception)
     {
-        if (exception is ApiException apiEx) exception = apiEx.InnerException;
+        IUser? user = null;
+        switch (exception)
+        {
+            case ApiException apiEx:
+                exception = apiEx.InnerException;
+                user = apiEx.LoggedUser;
+                break;
+            case JobException jobException:
+                exception = jobException.InnerException;
+                user = jobException.LoggedUser;
+                break;
+        }
 
         var isWarning = exception != null && LoggingHelper.IsWarning(exception);
-        var data = CreateWrapper(isWarning, source, message, exception);
-        await AuditLogWriteManager.StoreAsync(data);
+        var logRequest = CreateLogRequest(isWarning, source, message, exception, user);
+        await SendLogRequestAsync(logRequest);
     }
 
-    private static AuditLogDataWrapper CreateWrapper(bool isWarning, string source, string message, Exception? exception)
+    private static LogRequest CreateLogRequest(bool isWarning, string source, string message, Exception? exception, IUser? user)
     {
-        var type = isWarning ? AuditLogItemType.Warning : AuditLogItemType.Error;
         var severity = isWarning ? LogSeverity.Warning : LogSeverity.Error;
-        var data = new LogMessage(severity, source, message, exception).ToString(padSource: 50, prependTimestamp: false);
+        var logMessage = new LogMessage(severity, source, message, exception)
+            .ToString(padSource: 50, prependTimestamp: false);
 
-        return new AuditLogDataWrapper(type, data);
+        return new LogRequest
+        {
+            Type = isWarning ? LogType.Warning : LogType.Error,
+            LogMessage = new LogMessageRequest
+            {
+                Message = logMessage,
+                Severity = severity
+            },
+            CreatedAt = DateTime.UtcNow,
+            UserId = user?.Id.ToString()
+        };
+    }
+
+    private async Task SendLogRequestAsync(LogRequest request)
+    {
+        using var scope = ServiceProvider.CreateScope();
+
+        var client = scope.ServiceProvider.GetRequiredService<IAuditLogServiceClient>();
+        await client.CreateItemsAsync(new List<LogRequest> { request });
     }
 }

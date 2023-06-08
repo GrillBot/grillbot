d863049c919b221bfb1f18e0e947436bd7e3b7e4
Michal Halabica <m.halabica@gmail.com>
2023-06-08 13:32:33 +0200

    AuditOverwrites

--- src/GrillBot.App/Managers/AuditLogWriteManager.cs
+++ src/GrillBot.App/Managers/AuditLogWriteManager.cs
@@ -1,8 +1,6 @@
-﻿using GrillBot.Data.Models.AuditLog;
+﻿namespace GrillBot.App.Managers;
 
-namespace GrillBot.App.Managers;
-
-public class AuditLogWriteManager
+public static class AuditLogWriteManager
 {
     public static JsonSerializerSettings SerializerSettings => new()
     {
@@ -10,39 +8,4 @@ public class AuditLogWriteManager
         Formatting = Formatting.None,
         NullValueHandling = NullValueHandling.Ignore
     };
-
-    private GrillBotDatabaseBuilder DatabaseBuilder { get; }
-
-    public AuditLogWriteManager(GrillBotDatabaseBuilder databaseBuilder)
-    {
-        DatabaseBuilder = databaseBuilder;
-    }
-
-    public async Task StoreAsync(List<AuditLogDataWrapper> items)
-    {
-        await using var repository = DatabaseBuilder.CreateRepository();
-
-        foreach (var item in items.Where(o => o.Guild != null).DistinctBy(o => o.Guild.Id))
-            await repository.Guild.GetOrCreateGuildAsync(item.Guild);
-
-        foreach (var item in items.Where(o => o.Guild != null && o.Channel != null).DistinctBy(o => o.Channel.Id))
-        {
-            var guildChannel = await item.Guild.GetChannelAsync(item.Channel.Id);
-            if (guildChannel != null)
-                await repository.Channel.GetOrCreateChannelAsync(guildChannel);
-        }
-
-        foreach (var item in items.Where(o => o.ProcessedUser != null).DistinctBy(o => o.ProcessedUser.Id))
-        {
-            await repository.User.GetOrCreateUserAsync(item.ProcessedUser);
-            if (item.Guild == null) continue;
-
-            var guildUser = item.ProcessedUser as IGuildUser ?? await item.Guild.GetUserAsync(item.ProcessedUser.Id);
-            if (guildUser != null)
-                await repository.GuildUser.GetOrCreateGuildUserAsync(guildUser);
-        }
-
-        await repository.AddCollectionAsync(items.Select(o => o.ToEntity(SerializerSettings)));
-        await repository.CommitAsync();
-    }
 }
--- src/GrillBot.Database/Services/Repository/AuditLogRepository.cs
+++ src/GrillBot.Database/Services/Repository/AuditLogRepository.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Discord;
 using GrillBot.Core.Database;
 using GrillBot.Core.Database.Repository;
-using GrillBot.Core.Extensions;
 using GrillBot.Core.Managers.Performance;
 using GrillBot.Core.Models.Pagination;
 using GrillBot.Database.Entity;
@@ -18,34 +16,6 @@ public class AuditLogRepository : RepositoryBase<GrillBotContext>
 {
     public AuditLogRepository(GrillBotContext context, ICounterManager counter) : base(context, counter)
     {
-    }
-
-    public async Task<List<ulong>> GetDiscordAuditLogIdsAsync(IGuild? guild, IChannel? channel, AuditLogItemType[]? types, DateTime after)
-    {
-        using (CreateCounter())
-        {
-            var query = Context.AuditLogs.AsNoTracking()
-                .Where(o => o.DiscordAuditLogItemId != null && o.CreatedAt >= after);
-
-            if (guild != null)
-                query = query.Where(o => o.GuildId == guild.Id.ToString());
-
-            if (channel != null)
-                query = query.Where(o => o.ChannelId == channel.Id.ToString());
-
-            if (types?.Length > 0)
-                query = query.Where(o => types.Contains(o.Type));
-
-            var ids = await query
-                .Select(o => o.DiscordAuditLogItemId!)
-                .ToListAsync();
-
-            return ids
-                .SelectMany(o => o.Split(','))
-                .Select(o => o.Trim().ToUlong())
-                .Distinct()
-                .ToList();
-        }
     }
 
     public async Task<bool> ExistsExpiredItemAsync(DateTime expiredAt)

f41e1f19845628893f354c40935a9b596f91c047
Michal Halabica <m.halabica@gmail.com>
2023-10-10 15:59:46 +0200

    Improved performance of loading audit log records

--- src/GrillBot.App/Actions/Api/V1/AuditLog/GetAuditLogList.cs
+++ src/GrillBot.App/Actions/Api/V1/AuditLog/GetAuditLogList.cs
@@ -25,6 +25,10 @@ public class GetAuditLogList : ApiAction
     private IAuditLogServiceClient AuditLogServiceClient { get; }
     private IDiscordClient DiscordClient { get; }
 
+    private Dictionary<string, Database.Entity.User> CachedUsers { get; } = new();
+    private Dictionary<string, Database.Entity.Guild> CachedGuilds { get; } = new();
+    private Dictionary<string, Dictionary<string, Database.Entity.GuildChannel>> CachedChannels { get; } = new(); // Dictionary<GuildId, Dictionary<ChannelId, Channel>>
+
     public GetAuditLogList(ApiRequestContext apiContext, GrillBotDatabaseBuilder databaseBuilder, IMapper mapper, IFileServiceClient fileServiceClient, IAuditLogServiceClient auditLogServiceClient,
         IDiscordClient discordClient) : base(apiContext)
     {
@@ -93,14 +97,14 @@ public class GetAuditLogList : ApiAction
 
         if (!string.IsNullOrEmpty(item.GuildId))
         {
-            var guild = await repository.Guild.FindGuildByIdAsync(item.GuildId.ToUlong(), true);
+            var guild = await ResolveGuildAsync(repository, item.GuildId);
             if (guild is not null)
             {
                 result.Guild = Mapper.Map<Data.Models.API.Guilds.Guild>(guild);
 
                 if (!string.IsNullOrEmpty(item.ChannelId))
                 {
-                    var channel = await repository.Channel.FindChannelByIdAsync(item.ChannelId.ToUlong(), guild.Id.ToUlong(), true, includeDeleted: true);
+                    var channel = await ResolveChannelAsync(repository, item.GuildId, item.ChannelId);
                     if (channel is not null)
                         result.Channel = Mapper.Map<Data.Models.API.Channels.Channel>(channel);
                 }
@@ -109,7 +113,7 @@ public class GetAuditLogList : ApiAction
 
         if (!string.IsNullOrEmpty(item.UserId))
         {
-            var user = await repository.User.FindUserByIdAsync(item.UserId.ToUlong(), disableTracking: true);
+            var user = await ResolveUserAsync(repository, item.UserId);
             if (user is not null)
                 result.User = Mapper.Map<Data.Models.API.Users.User>(user);
         }
@@ -142,7 +146,7 @@ public class GetAuditLogList : ApiAction
                 {
                     var preview = jsonElement.Deserialize<SearchModels.OverwritePreview>(options)!;
                     var role = preview.TargetType == PermissionTarget.Role ? await DiscordClient.FindRoleAsync(preview.TargetId.ToUlong()) : null;
-                    var user = preview.TargetType == PermissionTarget.User ? await repository.User.FindUserByIdAsync(preview.TargetId.ToUlong()) : null;
+                    var user = preview.TargetType == PermissionTarget.User ? await ResolveUserAsync(repository, preview.TargetId) : null;
 
                     return new OverwritePreview
                     {
@@ -156,7 +160,7 @@ public class GetAuditLogList : ApiAction
                 {
                     var preview = jsonElement.Deserialize<SearchModels.OverwriteUpdatedPreview>(options)!;
                     var role = preview.TargetType == PermissionTarget.Role ? await DiscordClient.FindRoleAsync(preview.TargetId.ToUlong()) : null;
-                    var user = preview.TargetType == PermissionTarget.User ? await repository.User.FindUserByIdAsync(preview.TargetId.ToUlong(), disableTracking: true) : null;
+                    var user = preview.TargetType == PermissionTarget.User ? await ResolveUserAsync(repository, preview.TargetId) : null;
 
                     return new OverwriteUpdatedPreview
                     {
@@ -167,7 +171,7 @@ public class GetAuditLogList : ApiAction
             case LogType.Unban:
                 {
                     var preview = jsonElement.Deserialize<SearchModels.UnbanPreview>(options)!;
-                    var user = await repository.User.FindUserByIdAsync(preview.UserId.ToUlong(), disableTracking: true);
+                    var user = await ResolveUserAsync(repository, preview.UserId);
 
                     return new UnbanPreview
                     {
@@ -177,7 +181,7 @@ public class GetAuditLogList : ApiAction
             case LogType.MemberUpdated:
                 {
                     var preview = jsonElement.Deserialize<SearchModels.MemberUpdatedPreview>(options)!;
-                    var user = await repository.User.FindUserByIdAsync(preview.UserId.ToUlong(), disableTracking: true);
+                    var user = await ResolveUserAsync(repository, preview.UserId);
 
                     return new MemberUpdatedPreview
                     {
@@ -191,7 +195,7 @@ public class GetAuditLogList : ApiAction
             case LogType.MemberRoleUpdated:
                 {
                     var preview = jsonElement.Deserialize<SearchModels.MemberRoleUpdatedPreview>(options)!;
-                    var user = await repository.User.FindUserByIdAsync(preview.UserId.ToUlong(), disableTracking: true);
+                    var user = await ResolveUserAsync(repository, preview.UserId);
 
                     return new MemberRoleUpdatedPreview
                     {
@@ -204,7 +208,7 @@ public class GetAuditLogList : ApiAction
             case LogType.UserLeft:
                 {
                     var preview = jsonElement.Deserialize<SearchModels.UserLeftPreview>(options)!;
-                    var user = await repository.User.FindUserByIdAsync(preview.UserId.ToUlong(), disableTracking: true);
+                    var user = await ResolveUserAsync(repository, preview.UserId);
 
                     return new UserLeftPreview
                     {
@@ -221,7 +225,7 @@ public class GetAuditLogList : ApiAction
             case LogType.MessageDeleted:
                 {
                     var preview = jsonElement.Deserialize<SearchModels.MessageDeletedPreview>(options)!;
-                    var user = await repository.User.FindUserByIdAsync(preview.AuthorId.ToUlong(), disableTracking: true);
+                    var user = await ResolveUserAsync(repository, preview.AuthorId);
 
                     return new MessageDeletedPreview
                     {
@@ -258,5 +262,47 @@ public class GetAuditLogList : ApiAction
             Link = link!,
             Size = file.Size
         };
+    }
+
+    private async Task<Database.Entity.User?> ResolveUserAsync(GrillBotRepository repository, string userId)
+    {
+        if (CachedUsers.TryGetValue(userId, out var user))
+            return user;
+
+        user = await repository.User.FindUserByIdAsync(userId.ToUlong(), disableTracking: true);
+        if (user is null)
+            return null;
+
+        CachedUsers.Add(user.Id, user);
+        return user;
+    }
+
+    private async Task<Database.Entity.Guild?> ResolveGuildAsync(GrillBotRepository repository, string guildId)
+    {
+        if (CachedGuilds.TryGetValue(guildId, out var guild))
+            return guild;
+
+        guild = await repository.Guild.FindGuildByIdAsync(guildId.ToUlong(), true);
+        if (guild is null)
+            return null;
+
+        CachedGuilds.Add(guild.Id, guild);
+        return guild;
+    }
+
+    private async Task<Database.Entity.GuildChannel?> ResolveChannelAsync(GrillBotRepository repository, string guildId, string channelId)
+    {
+        if (CachedChannels.TryGetValue(guildId, out var guildChannels) && guildChannels.TryGetValue(channelId, out var guildChannel))
+            return guildChannel;
+
+        guildChannel = await repository.Channel.FindChannelByIdAsync(channelId.ToUlong(), guildId.ToUlong(), true, includeDeleted: true);
+        if (guildChannel is null)
+            return null;
+
+        if (!CachedChannels.ContainsKey(guildId))
+            CachedChannels.Add(guildId, new Dictionary<string, Database.Entity.GuildChannel>());
+
+        CachedChannels[guildId].Add(channelId, guildChannel);
+        return guildChannel;
     }
 }

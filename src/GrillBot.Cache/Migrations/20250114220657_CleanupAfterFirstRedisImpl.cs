0f6664f1f5e73417fae6a17f4ccbf7d2114c5b62
Michal Halabica <m.halabica@gmail.com>
2025-01-14 23:12:52 +0100

    Dropped old table in the cache database

---
+++ src/GrillBot.Cache/Migrations/20250114220657_CleanupAfterFirstRedisImpl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrillBot.Cache.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupAfterFirstRedisImpl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataCache");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DataCache",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    ValidTo = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataCache", x => x.Key);
+                });
+        }
+    }
+}

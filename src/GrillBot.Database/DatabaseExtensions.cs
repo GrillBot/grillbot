92124c6643542bcee4678234caf7e34291735fde
Michal Halabica <m.halabica@gmail.com>
2025-04-25 22:57:23 +0200

    Handle restart policy in the postgres EF COre

--- src/GrillBot.Database/DatabaseExtensions.cs
+++ src/GrillBot.Database/DatabaseExtensions.cs
@@ -7,10 +7,17 @@ namespace GrillBot.Database;
 
 public static class DatabaseExtensions
 {
+    private static string SQL_RESTART_ERROR_CODE = "57P01";
+
     public static IServiceCollection AddDatabase(this IServiceCollection services, string connectionString)
     {
         return services
-            .AddDatabaseContext<GrillBotContext>(b => b.UseNpgsql(connectionString))
+            .AddDatabaseContext<GrillBotContext>(
+                b => b.UseNpgsql(
+                    connectionString,
+                    opt => opt.EnableRetryOnFailure([SQL_RESTART_ERROR_CODE])
+                )
+            )
             .AddSingleton<GrillBotDatabaseBuilder>();
     }
 }

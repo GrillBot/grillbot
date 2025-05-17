c961238db33df7637e8e88ed18df2bb9b86c6dd6
Michal Halabica <m.halabica@gmail.com>
2025-05-17 09:50:01 +0200

    Small code refactoring

--- src/GrillBot.App/Controllers/ServiceControllers/EmoteController.cs
+++ src/GrillBot.App/Controllers/ServiceControllers/EmoteController.cs
@@ -18,51 +18,51 @@ namespace GrillBot.App.Controllers.ServiceControllers;
 public class EmoteController(IServiceProvider serviceProvider) : ServiceControllerBase<IEmoteServiceClient>(serviceProvider)
 {
     [HttpPost("statistics-list")]
-    [ProducesResponseType(typeof(PaginatedResponse<EmoteStatisticsItem>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<PaginatedResponse<EmoteStatisticsItem>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     public Task<IActionResult> GetEmoteStatisticsListAsync(EmoteStatisticsListRequest request)
         => ExecuteAsync(async (client, ctx) => await client.GetEmoteStatisticsListAsync(request, ctx.CancellationToken), request);
 
     [HttpGet("{guildId}/{emoteId}")]
-    [ProducesResponseType(typeof(EmoteInfo), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<EmoteInfo>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     public Task<IActionResult> GetEmoteInfoAsync(string guildId, string emoteId)
         => ExecuteAsync(async (client, ctx) => await client.GetEmoteInfoAsync(guildId, emoteId, ctx.CancellationToken));
 
     [HttpPut("merge/{guildId}/{sourceEmoteId}/{destinationEmoteId}")]
-    [ProducesResponseType(typeof(MergeStatisticsResult), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<MergeStatisticsResult>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     public Task<IActionResult> MergeStatisticsAsync(string guildId, string sourceEmoteId, string destinationEmoteId)
         => ExecuteAsync(async (client, ctx) => await client.MergeStatisticsAsync(guildId, sourceEmoteId, destinationEmoteId, ctx.AuthorizationToken, ctx.CancellationToken));
 
     [HttpPost("usage-list")]
-    [ProducesResponseType(typeof(PaginatedResponse<EmoteUserUsageItem>), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<PaginatedResponse<EmoteUserUsageItem>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     public Task<IActionResult> GetUserEmoteUsageListAsync(EmoteUserUsageListRequest request)
         => ExecuteAsync(async (client, ctx) => await client.GetUserEmoteUsageListAsync(request, ctx.CancellationToken), request);
 
     [HttpGet("supported-emotes")]
-    [ProducesResponseType(typeof(List<EmoteDefinition>), StatusCodes.Status200OK)]
+    [ProducesResponseType<List<EmoteDefinition>>(StatusCodes.Status200OK)]
     public Task<IActionResult> GetSupportedEmotesListAsync([FromQuery] string? guildId = null)
         => ExecuteAsync(async (client, ctx) => await client.GetSupportedEmotesListAsync(guildId, ctx.CancellationToken));
 
     [HttpDelete("supported-emotes/{guildId}/{emoteId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task<IActionResult> DeleteSupportedEmotesAsync(string guildId, string emoteId)
         => ExecuteAsync(async (client, ctx) => await client.DeleteSupportedEmoteAsync(guildId, emoteId, ctx.CancellationToken));
 
     [HttpDelete("{guildId}/{emoteId}")]
-    [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<int>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public Task<IActionResult> DeleteStatisticsAsync(string guildId, string emoteId, [FromQuery] string? userId = null)
         => ExecuteAsync(async (client, ctx) => await client.DeleteStatisticsAsync(guildId, emoteId, userId, ctx.AuthorizationToken, ctx.CancellationToken));
 
     [HttpPost("emote-suggestions/list")]
     [ProducesResponseType<PaginatedResponse<EmoteSuggestionItem>>(StatusCodes.Status200OK)]
-    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ValidationProblemDetails>(StatusCodes.Status400BadRequest)]
     public Task<IActionResult> GetEmoteSuggestionsListAsync(EmoteSuggestionsListRequest request)
         => ExecuteAsync((client, ctx) => client.GetEmoteSuggestionsAsync(request, ctx.CancellationToken), request);
 
